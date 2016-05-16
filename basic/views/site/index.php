a734729faa51c3fd29dc0d8db30e57eb47c28325
Eric Sills <eric.d.sills@gmail.com>
2016-05-16 08:14:18 -0400

    updated buttons

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -24,19 +24,19 @@ $this->title = 'OIT Research Services Application';
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-4 col-sm-6">
                 <h2>Unclaimed Storage</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
-		<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+		<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Claim Storage</a></p>
 <!---
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
 --->
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-4 col-sm-6">
                 <h2>Manage Storage</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -44,9 +44,12 @@ $this->title = 'OIT Research Services Application';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
+		<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Manage Storage</a></p>
+<!---
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+--->
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-4 col-sm-6">
                 <h2>Manage HPC Project</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -54,7 +57,10 @@ $this->title = 'OIT Research Services Application';
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
+		<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Manage/Request HPC Project</a></p>
+<!---
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+--->
             </div>
         </div>
 
